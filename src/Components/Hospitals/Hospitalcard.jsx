96e097e61dcfa5b4c6abdb82ee9c70a4cb08bd7f
Nimesh <nimesh.sakhiya2003@gmail.com>
2023-01-21 12:28:46 +0530

    hospitals list added

---
+++ src/Components/Hospitals/Hospitalcard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./hospitals.css";
+
+function Hospitalcard() {
+  return (
+    <div>
+      <div className="google_card">
+        <div class="card">
+          <iframe
+            className="Map"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3767.745838992304!2d72.87241691482411!3d19.20630028701321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1674281458322!5m2!1sen!2sin"
+          ></iframe>
+          <div class="container">
+            <h4>
+              <b>Name</b><hr></hr>
+            </h4>
+            <div className="hospital_detail">
+              <p>Address:</p>
+              <p>city:</p>
+              <p>pincode:</p>
+              <p>no.of beds:</p>
+              <p>no.of doctors:</p>
+              <p>Contact no.:</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hospitalcard;
--- src/Components/Hospitals/Hospitals_list.jsx
+++ src/Components/Hospitals/Hospitals_list.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "../DoctorDashboard/Navbar";
+import Hospitalcard from "./Hospitalcard";
+import "./hospitals.css";
 
 function Hospitals_list() {
   const list = [
@@ -13,13 +16,30 @@ function Hospitals_list() {
   ];
 
   return (
-    <div>
-      <ul>
+    <>
+      <Navbar />
+      <div>
+        {/* <ul>
         {list.map((item) => (
           <li key={item.id}>{item.name}</li>
         ))}
-      </ul>
-    </div>
+      </ul> */}
+        <div className="hospitals_page_headings">
+          <span>Hospitals Near You</span>
+          <span>Medicals Near You</span>
+        </div>
+        <div className="generic_data">
+          <div className="hospitals_list">
+            
+            <Hospitalcard/>
+          </div>
+          <div className="medicals_list">
+            
+          <Hospitalcard/>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
