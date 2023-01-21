2751ee05bbac2b5c84f35aa74b359f311d284d92
Nimesh <nimesh.sakhiya2003@gmail.com>
2023-01-21 06:05:01 +0530

    dashboard changes

--- src/Components/Appointmentcard/AppointmentCard.jsx
+++ src/Components/Appointmentcard/AppointmentCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import "../PatientDashboardCss/PatientHome.css"
 import swal from 'sweetalert';
+import {  useLocation } from 'react-router-dom';
 
 function AppointmentCard() {
-    console.log(window.location)
+  
+  var btns = false;
+    // if(window.location.href=="http://localhost:3000/patient/home"){
+    //   setbtns(false)
+    // }else{
+    //   setbtns(true)
+    // }
+
+  const location = useLocation();
+  console.log(location.pathname) 
+  if(location.pathname=="/doctor/home"){
+    btns = true;
+  }else{
+    btns = false;
+  }
+
   const AppointmentModal = ( ) =>{
     swal({
       text: "Hello world!",
@@ -11,14 +27,31 @@ function AppointmentCard() {
   }
     return (
         <div onClick={()=>{AppointmentModal()}} className='smAppointmentCard'>
-          <span>Patient Name</span>
-          <span>Date </span>
-          <span>
-            <button className='approve'>Approve</button>
-            <button className='reject'>Reject</button>
-          </span>
+           {
+              btns
+              ?
+                <div className='appointment_card'>
+                  <div className='patient_name'>
+                    <p>patient Name</p>
+                    <p>Date</p>
+                  </div>
+                  <div className='appointment_btns'>
+                    <button className='approve'>Approve</button>
+                    <button className='reject'>Reject</button>
+                  </div>
+                </div>
+              :<>
+                <span>Patient Name</span>
+                <span>Date </span>
+                 <span>
+                  <button className='approve'>Approve</button>
+                  <button className='reject'>Reject</button>
+                </span>
+                <p>Status</p></>
+          }
+          
         </div>
       )
 }
 
-export default AppointmentCard
+export default AppointmentCard;
